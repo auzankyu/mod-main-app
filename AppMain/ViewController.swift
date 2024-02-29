5a5d7651051297f8c4d2735cae9c77d72d6b97d1
Auzan Lazuardi <auzanlazuardi@gmail.com>
2024-02-29 22:32:56 +0700

    Initial Commit

---
+++ AppMain/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppMain
+//
+//  Created by Auzan Lazuardi on 29/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
